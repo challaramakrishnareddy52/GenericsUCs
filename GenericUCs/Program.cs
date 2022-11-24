adafa552521925b9a637a31b74b45cdd26f4023f
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-24 14:32:59 +0530

    Welcome to Generic Use Cases

---
+++ GenericUCs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericsUCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Use Case's ");
+            FindMaximum.FindMaxValue(10, 20, 30);
+        }
+    }
+}
