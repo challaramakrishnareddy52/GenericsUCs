feb97156b9c83d2de80525cbca7aad7039258708
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-24 14:50:00 +0530

    Finding Max of 3  Strings

--- GenericUCs/FindMaximum.cs
+++ GenericUCs/FindMaximum.cs
@@ -8,7 +8,7 @@ namespace GenericsUCs
 {
     internal class FindMaximum
     {
-        public static void FindMaxValue(int first, int second, int third)       //UC1 finding max of 3 numbers.
+        public static void FindMaxValue(int first, int second, int third)       //UC1 finding max of 3 Int numbers.
         {
             if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
             {
@@ -25,7 +25,24 @@ namespace GenericsUCs
             else
                 Console.WriteLine("All Numbers are Equal! ");
         }
-        public static void FindMaxValue(float first, float second, float third)       //UC2 finding max out of 3 float.
+        public static void FindMaxValue(float first, float second, float third)       //UC2 finding max of 3 float
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+        public static void FindMaxValue(string first, string second, string third)      // UC3 finding max of 3 string 
         {
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
             {
--- GenericUCs/Program.cs
+++ GenericUCs/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Welcome to Generic Use Case's ");
             FindMaximum.FindMaxValue(10, 20, 30);
             FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);
+            FindMaximum.FindMaxValue("abc", "pqr", "xyz");
         }
     }
 }
